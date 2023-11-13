a46b0976572c4cd32abd49bada79d507737c952b
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-11-14 01:10:00 +0300

    index.html and script.js files  created

---
+++ public/script.js
@@ -0,0 +1,43 @@
+
+
+
+window.onload = function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
+    flowField.animate();
+}
+
+//Encapsulation
+class FlowFieldEffect {
+    #ctx;
+    #width;
+    #height;
+    constructor(ctx , width , height){
+        this.#ctx = ctx;
+        this.#width = width;
+        this.#height = height;
+        console.log("effect loaded");
+    }
+    //Private methods
+    #draw(x , y) {
+        const length = 300;
+        this.#ctx.strokeStyle = 'white';
+        this.#ctx.beginPath();
+        this.#ctx.moveTo(x, y);
+        this.#ctx.lineTo(x + length , y + length);
+        
+    }
+
+    //Public Methods
+    animate(){
+        this.#draw(this.x , this.y);
+        this.x += 0.5;
+        this.y += 2.5;
+        console.log("animating...");
+        requestAnimationFrame(this.animate.bind(this));
+    }
+}
+
