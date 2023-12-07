7e644dc5e451cb55895248fcc1acf9c88c7c98e0
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-12-07 22:41:25 +0300

    create signin route api

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,113 +2,52 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const user = require("../models/user.js");
 
-const signup = async (req, res, next) => {
-  const {username , email, password} = req.body;
-  const hashPassword = bcrypt.hashSync(password, 10)
-  try{
-    const data = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: ha
-    });
-  
-    user.save((err, user) => {
-      if (err) {
-        res.status(500)
-          .send({
-            message: err
-          });
-        return;
-      } else {
-        res.status(200)
-          .send({
-            message: "User Registered successfully"
-          })
-      }
-    });
-  }catch(err){
-    console.log(err);
-  }
-};
-
-// const signin = (req, res) => {
-//   User.findOne({
-//       email: req.body.email
-//     })
-//     .exec((err, user) => {
-//       if (err) {
-//         res.status(500)
-//           .send({
-//             message: err
-//           });
-//         return;
-//       }
-//       if (!user) {
-//         return res.status(404)
-//           .send({
-//             message: "User Not found."
-//           });
-//       }
-
-//       //comparing passwords
-//       const passwordIsValid = bcrypt.compareSync(
-//         req.body.password,
-//         user.password
-//       );
-//       // checking if password was valid and send response accordingly
-//       if (!passwordIsValid) {
-//         return res.status(401)
-//           .send({
-//             accessToken: null,
-//             message: "Invalid Password!"
-//           });
-//       }
-//       //signing token with user id
-//       const token = jwt.sign({
-//         id: user.id
-//       }, process.env.API_SECRET, {
-//         expiresIn: 86400
-//       });
-
-//       //responding to client request with user profile success message and  access token .
-//       res.status(200)
-//         .send({
-//           user: {
-//             id: user._id,
-//             email: user.email,
-//             fullName: user.fullName,
-//           },
-//           message: "Login successfull",
-//           accessToken: token,
-//         });
-//     });
-// };
-
 const signIn = async (req, res, next) => {
-  const {email, password} = req.body;
-  try{
-     const validateUser = User.findOne({email, password})
-     if(!validateUser) return next(err);
-     const validPassword = bcrypt.compareSync(password, validateUser.password)
-     if(!validPassword) return next(err);
-     const token = jwt.sign({id: validateUser._id}, process.env.API_SECRET, {expiresIn: 86400})
-     res.status(200).json({user: validateUser, token: token}).cookie("token", token);
-    }catch(error){
-      next(error);
+  let { email, password } = req.body;
+  console.log(email, password);
+ 
+  let existingUser;
+  try {
+    existingUser = await user.findOne({ email: email });
+   console.log("existing user", existingUser);
+  } catch {
+    const error = new Error("Error! Something went wrong.");
+    return next(error);
   }
-}
-
+  if (!existingUser || existingUser.password != password) {
+    const error = Error("Wrong details please check at once");
+    return next(error);
+  }
+  let token;
+  try {
+    //Creating jwt token
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      "secretkeyappearshere",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    console.log(err);
+    const error = new Error("Error! Something went wrong.");
+    return next(error);
+  }
+ 
+  res
+    .status(200)
+    .json({
+      success: true,
+      data: {
+        userId: existingUser.id,
+        email: existingUser.email,
+        token: token,
+      },
+    });
+};
 const getUser = (req, res) => {
     res.status(200).json({message : `auth controller i working`})
 }
 
-const updateUser = (req, res) => {
-    res.status(200).json({message : `Update user ${req.params.id}`})
-}
 
-const deleteUser = (req, res) => {
-    res.status(200).json({message : `Delete user ${req.params.id}`})
-}
 const createUser = async (req, res , next) => {
 
   const {username , email, password} = req.body;
@@ -132,6 +71,5 @@ const createUser = async (req, res , next) => {
 module.exports = {
     getUser,
     createUser,
-    signup,
-    updateUser,
-    deleteUser}
+    signIn,
+   }
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,11 +1,12 @@
 
 
 const express = require("express");
-const { signup,  getUser, createUser } = require("../controllers/auth");
+const { signup,  getUser, createUser, signIn } = require("../controllers/auth");
 const router = express.Router();
 
 
 router.post("/signup", createUser);
+router.post("/signin", signIn)
 
 
 module.exports = router;
