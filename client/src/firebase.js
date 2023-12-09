0d1ff3f02f90b9618eddd78644d6fe106907be90
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-12-09 15:37:04 +0300

    firebase-OAuth

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUObNtwMySvBrTFOg3QalF0_m5EPPbXNI",
+  authDomain: "mern-artgallery.firebaseapp.com",
+  projectId: "mern-artgallery",
+  storageBucket: "mern-artgallery.appspot.com",
+  messagingSenderId: "990285991889",
+  appId: "1:990285991889:web:669a17d9127c869ff95af0"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
