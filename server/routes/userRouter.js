c5742350b847c03a290a787bb92f915ad53dcd3e
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-12-21 16:34:12 +0300

    logout - getLoginStatus controller

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,13 +3,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser, createUser, deleteUser, getUser, getUsers, updateUser } = require("../controllers/user.js");
-
+const { registerUser, loginUser, logout, getUser, getLoginStatus } = require("../controllers/user.js");
+const {protect} = require("../middleware/authMiddleware.js")
 
 
 // router.route("/").get(getUsers).post(createUser);
 // router.route("/:id").get(getUser).delete(deleteUser).put(updateUser);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.get("/logout", logout);
+router.get("/getUser" ,protect,  getUser);
+router.get("/getLoginStatus", getLoginStatus);
 module.exports = router;
