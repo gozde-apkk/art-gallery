7e24b3ea8ca325eae6d422cbfab63475bb03c498
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-12-22 22:06:05 +0300

    login and register pages created

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,113 +1,130 @@
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+  MDBBtn,
+  MDBIcon,
+  MDBInput,
+  MDBCheckbox
+}
+from 'mdb-react-ui-kit';
 
+function App() {
 
+  const [justifyActive, setJustifyActive] = useState('tab1');;
 
-
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-import OAuth from '../../components/oauth/OAuth';
-
-
-const Login = () => {
-
-    
-    const navigate = useNavigate();
-    const [formData , setFormData] = useState({});
- const [ error , setError] = useState({});
-
-
-    const handleChange = (e) =>{
-        setFormData({
-            ...formData,
-            [e.target.id] : e.target.value
-        })
+  const handleJustifyClick = (value) => {
+    if (value === justifyActive) {
+      return;
     }
-//    const user = localStorage.getItem('persist:root')
-    const handleSubmit = async (e) => {
-        try{
-          e.preventDefault ();
-        //  dispatch(signInStart());
-          const res = await fetch('http://localhost:4000/api/auth/login', {
-            method : 'POST',
-            headers : {
-                "Content-Type" : "application/json"
-            },
-            body : JSON.stringify(formData),
-          });
-          const data = await res.json();
-          if(data.success === false){
-        //    dispatch(signInFailure(data.message));
-            return
-          }
-        //   dispatch(signInSuccess(data)); 
-        //   dispatch({type : 'AUTH', payload : data})
-         navigate('/profile');
-        }catch(error){
-        //    dispatch(signInFailure(error.message));
-        }
+
+    setJustifyActive(value);
+  };
+
   return (
-    <div className='h-full bg-gray-900 flex justify-center text-gray-100'> 
-         <main  className=" flex flex-col items-center justify-center ">
-            <div className="max-w-sm w-full text-gray-300 space-y-7">
-                <div style={{margin:"16px"}} className="text-center flex h-[87px] justify-center items-center">
-                   <img style={{width: "30px"}} className=' '  src='/icon/whitelogo.png'/>
-                    <div className="mt-3 w-[151px] ">
-                        <h3 style={{fontSize: "xx-large"}} className="text-gray-100  h-[56px] font-bold sm:text-3xl">Sign in</h3>
-                    </div>
-                </div>
-                <form
-                    // onSubmit={handleSubmit}
-                    className="space-y-5"
-                >
-                    <div>
-                        <label className="font-medium">
-                            Email
-                        </label>
-                        <input 
-                        onChange={handleChange}
-                            type="email"
-                            id='email'
-                            required
-                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                        />
-                    </div>
-                    <div>
-                        <label className="font-medium">
-                            Password
-                        </label>
-                        <input
-                             onChange={handleChange}
-                             id='password'
-                            type="password"
-                            required
-                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                        />
-                    </div>
-                    <div style={{marginTop: "13px"}} className="flex items-center justify-between text-sm">
-                        <div className="flex items-center gap-x-3">
-                            <input type="checkbox" id="remember-me-checkbox" className="checkbox-item peer hidden" />
-                            <label
-                                htmlFor="remember-me-checkbox"
-                                className="relative flex w-5 h-5 bg-white peer-checked:bg-indigo-600 rounded-md border ring-offset-2 ring-indigo-600 duration-150 peer-active:ring cursor-pointer after:absolute after:inset-x-0 after:top-[3px] after:m-auto after:w-1.5 after:h-2.5 after:border-r-2 after:border-b-2 after:border-white after:rotate-45"
-                            >
-                            </label>
-                            <span>Remember me</span>
-                        </div>
-                        <a href="javascript:void(0)" className="text-center ">Forgot password?</a>
-                    </div>
-                    <button
-                        className="w-full px-4 py-2 text-white font-medium border-collapse rounded-lg bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 duration-150"
-                    >
-                        SIGN IN
-                    </button>
-                </form>
-           <OAuth/>
-                <p style={{marginTop:"14px"}} className="text-center">Don't have an account? <a href="/sign-up" className="font-medium text-indigo-600 hover:text-indigo-500">Create Account</a></p>
+    <MDBContainer className="p-3 text-white  flex-column w-50">
+
+      <MDBTabs pills justify className='mb-3 d-flex flex-row justify-content-between'>
+        <MDBTabsItem>
+          <MDBTabsLink onClick={() => handleJustifyClick('tab1')} active={justifyActive === 'tab1'}>
+            Login
+          </MDBTabsLink>
+        </MDBTabsItem>
+        <MDBTabsItem>
+          <MDBTabsLink onClick={() => handleJustifyClick('tab2')} active={justifyActive === 'tab2'}>
+            Register
+          </MDBTabsLink>
+        </MDBTabsItem>
+      </MDBTabs>
+
+      <MDBTabsContent>
+
+        <MDBTabsPane show={justifyActive === 'tab1'}>
+
+          <div className="text-center mb-3">
+            <p>Sign in with:</p>
+
+            <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
+              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                <MDBIcon fab icon='facebook-f' size="sm"/>
+              </MDBBtn>
+
+              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                <MDBIcon fab icon='twitter' size="sm"/>
+              </MDBBtn>
+
+              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                <MDBIcon fab icon='google' size="sm"/>
+              </MDBBtn>
+
+              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                <MDBIcon fab icon='github' size="sm"/>
+              </MDBBtn>
             </div>
-            {error && <p>{error}</p>}
-        </main>
-    </div>
-  )
+
+            <p className="text-center mt-3">or:</p>
+          </div>
+
+          <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
+
+          <div className="d-flex justify-content-between mx-4 mb-4">
+            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+            <a href="!#">Forgot password?</a>
+          </div>
+
+          <MDBBtn className="mb-4 w-100">Sign in</MDBBtn>
+          <p className="text-center">Not a member? <a href="#!">Register</a></p>
+
+        </MDBTabsPane>
+
+        <MDBTabsPane show={justifyActive === 'tab2'}>
+
+          <div className="text-center mb-3">
+            <p>Sign un with:</p>
+
+            <div className='d-flex justify-content-between mx-auto' style={{width: '40%'}}>
+              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                <MDBIcon fab icon='facebook-f' size="sm"/>
+              </MDBBtn>
+
+              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                <MDBIcon fab icon='twitter' size="sm"/>
+              </MDBBtn>
+
+              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                <MDBIcon fab icon='google' size="sm"/>
+              </MDBBtn>
+
+              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
+                <MDBIcon fab icon='github' size="sm"/>
+              </MDBBtn>
+            </div>
+
+            <p className="text-center mt-3">or:</p>
+          </div>
+
+          <MDBInput wrapperClass='mb-4' label='Name' id='form1' type='text'/>
+          <MDBInput wrapperClass='mb-4' label='Username' id='form1' type='text'/>
+          <MDBInput wrapperClass='mb-4' label='Email' id='form1' type='email'/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='form1' type='password'/>
+
+          <div className='d-flex justify-content-center mb-4'>
+            <MDBCheckbox name='flexCheck' id='flexCheckDefault' label='I have read and agree to the terms' />
+          </div>
+
+          <MDBBtn className="mb-4 w-100">Sign up</MDBBtn>
+
+        </MDBTabsPane>
+
+      </MDBTabsContent>
+
+    </MDBContainer>
+  );
 }
-}
-export default Login;
+
+export default App;
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,143 +1,94 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  FacebookLoginButton,
+  InstagramLoginButton
+} from "react-social-login-buttons";
 
+class SignInForm extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      email: "",
+      password: ""
+    };
 
-import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
-import OAuth from '../../components/oauth/OAuth';
-// import {useDispatch, useSelector} from 'react-redux';
-// import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
-// import { setCredentials } from '../redux/user/authSlice';
-// import { setCredentialsAction } from '../redux/actions';
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-const Register = () =>  {
+  handleChange(event) {
+    let target = event.target;
+    let value = target.type === "checkbox" ? target.checked : target.value;
+    let name = target.name;
 
-    const [email , setEmail] = useState('');
-    
+    this.setState({
+      [name]: value
+    });
+  }
 
-    // const navigate = useNavigate();
-    // const [formData , setFormData] = useState({});
-    // const [error , setError] = useState({});
-    // // const {error} = useSelector(state => state.user);
-    // // const {user} = useSelector(state => state.auth);
-    // // const dispatch = useDispatch();
+  handleSubmit(event) {
+    event.preventDefault();
 
+    console.log("The form was submitted with the following data:");
+    console.log(this.state);
+  }
 
-    // const handleChange = (e) => {
-    //     setFormData({
-    //         ...formData,
-    //        [ e.target.id] : e.target.value,
-    //     })
-    // }
+  render() {
+    return (
+      <div className="mb-40">
+        <form className="formFields" onSubmit={this.handleSubmit}>
+          <div className="mb-10">
+            <label className="block transform-translate-x-1 text-md" htmlFor="email">
+              E-Mail Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="formFieldInput"
+              placeholder="Enter your email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </div>
 
-    // const  handleSubmit = async (e) => {
+          <div className="formField">
+            <label className="formFieldLabel" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="formFieldInput"
+              placeholder="Enter your password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </div>
 
-    //     try{
-    //         e.preventDefault();
-    //         // dispatch(signInStart());
-    //         const res = await fetch('http://localhost:4000/api/auth/signup', {
-    //             method : "POST",
-    //             headers : {
-    //                     "Content-Type": "application/json",
-    //             },
-    //             body: JSON.stringify(formData),
-    //         });
-    //         const data = await res.json();
-    //         if(data.success === false){
-    //             // dispatch(signInFailure(data.message));
-    //             return;
-    //         }
-    //         // dispatch(signInSuccess(data));
-    //         // dispatch(setCredentialsAction(data));
-    //        navigate('/profile');
-    //     }catch(error){
-    //         // dispatch(signInFailure(error.message));
-    //     }
-    // }
+          <div className="formField">
+            <button className="formFieldButton">Sign In</button>{" "}
+            <Link to="/" className="formFieldLink">
+              Create an account
+            </Link>
+          </div>
 
+          <div className="socialMediaButtons">
+            <div className="facebookButton">
+              <FacebookLoginButton onClick={() => alert("Hello")} />
+            </div>
 
-  return (
-    <div className='h-full  flex justify-center text-gray-200'> 
-    <main  className=" h-[583px] w-[350px] flex mt-1 flex-col items-center justify-center px-4">
-       <div className="max-w-sm mt-2 w-full text-gray-200 space-y-5">
-           <div  className="text-center flex h-[87px] justify-center items-center">
-             
-               <div className="mt-5 w-[251px] ">
-                   <h3 style={{fontSize: "xx-large"}} className=" h-[56px] font-WorkSans text-2xl sm:text-3xl">Create Account</h3>
-               </div>
-           </div>
-           <form
-            //    onSubmit={handleSubmit}
-               className=""
-               style={{position: "relative", backgroundColor:"hsl(0, 0%, 10%, 0.1)", 
-                       borderRadius: "10px", padding: "2.5rem 2.5rem", 
-                       border : "2px solid hsl(10, 0%, 90%)",}}
-           >
-               <div>
-                   <label className="font-medium">
-                      Username
-                   </label>
-                   <input
-                   
-                       type="name"
-                       id="username"
-                       required
-                       className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                   />
-               </div>
-               <div>
-                   <label className="font-medium">
-                      Email
-                   </label>
-                   <input
-                   
-                       type="email"
-                       id="email"
-                       required
-                       className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                   />
-               </div>
-               <div>
-                   <label className="font-medium">
-                       Password
-                   </label>
-                   <input
-                  
-                       type="password"
-                       id="password"
-                       required
-                       className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                   />
-               </div>
-               <div style={{marginTop: "13px"}} className="flex items-center justify-between text-sm">
-                   <div className="flex items-center gap-x-3">
-                       <input type="checkbox" id="remember-me-checkbox" className="checkbox-item peer hidden" />
-                       <label
-                           htmlFor="remember-me-checkbox"
-                           className="relative flex w-5 h-5 bg-white peer-checked:bg-indigo-600 rounded-md border ring-offset-2 ring-indigo-600 duration-150 peer-active:ring cursor-pointer after:absolute after:inset-x-0 after:top-[3px] after:m-auto after:w-1.5 after:h-2.5 after:border-r-2 after:border-b-2 after:border-white after:rotate-45"
-                       >
-                       </label>
-                       <span >Remember me</span>
-                   </div>
-                   <a href="javascript:void(0)" className="text-center text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-               </div>
-               <button 
-                   className="w-full px-4 py-2 text-white font-medium border-1 bg-indigo-600 hover:bg-indigo-500 rounded-lg duration-150"
-               >
-                  {/* {loading ? "Loading..." : ""} */}
-                  Sign Up...
- 
-               </button>
-           </form>
-
-           <p style={{marginTop:"14px"}} className="text-center">
-             Have an account?
-             <a href="/login" className="font-medium text-indigo-600 hover:text-indigo-500">Sign in</a>
-            </p>
-       </div>
-
-   </main>
-</div>
-  );
+            <div className="instagramButton">
+              <InstagramLoginButton onClick={() => alert("Hello")} />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Register;
+export default SignInForm;
