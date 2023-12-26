3913e87899559a8b9c4fda05f0cb67e4e55eed02
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-12-26 17:09:19 +0300

    auth reducer is ok

---
+++ client/src/redux/features/auth/authService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { FaFontAwesomeFlag } from "react-icons/fa";
+
+const BASE_URL = "http://localhost:4000/api/users";
+
+const LOGIN_URL = `${BASE_URL}/register`;
+
+//Register User
+
+const register = async (users) => {
+  try {
+    const res = await fetch(`${BASE_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(users),
+      withCredentials: true,
+    });
+    const data = await res.json();
+    if (data.success === false) {
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const authService = { register };
+
+export default authService;
--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,26 +1,63 @@
-import { createSlice } from '@reduxjs/toolkit';
+
+
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import authService from './authService';
+import { toast } from 'react-toastify';
 
 const initialState = {
-  userInfo: localStorage.getItem('userInfo')
-    ? JSON.parse(localStorage.getItem('userInfo'))
-    : null,
-};
+    isLoggedIn : false,
+    user : null,
+    isError: false,
+    isSuccess : false,
+    isLoading : false,
+    message : "",
+
+}
+
+export const registerUser = createAsyncThunk(
+    "auth/register",
+    async (user, thunkAPI) => {
+        try {
+            return await authService.register(user)
+        } catch (error) {
+            const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+        }
+    }
+)
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    setCredentials: (state, action) => {
-      state.userInfo = action.payload;
-      localStorage.setItem('userInfo', JSON.stringify(action.payload));
-    },
-    logout: (state, action) => {
-      state.userInfo = null;
-      localStorage.removeItem('userInfo');
-    },
+    RESET_AUTH(state) {
+      state.isLoggedIn = false;
+      state.user = null;
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
+    }
   },
+  extraReducers : (builder) => {
+    builder
+       .addCase(registerUser.pending, (state) => {
+         state.isLoading = true;
+       })
+       .addCase(registerUser.fulfilled, (state, action) => {
+         state.isLoading = false;
+         state.isSuccess = true;
+         state.user = action.payload;
+         toast.success("Registration Successful");
+       })
+       .addCase(registerUser.rejected, (state, action) => {
+         state.isLoading = false;
+         state.isError = true;
+         state.message = action.error;
+
+       })
+  }
 });
 
-export const { setCredentials, logout } = authSlice.actions;
+export const {} = authSlice.actions
 
-export default authSlice.reducer;
+export default authSlice.reducer
