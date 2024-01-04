8af8e693ab36beee6b9e401d5778522704a35832
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-04 21:03:08 +0300

    fetchProductById

--- client/src/redux/features/products/productApi.js
+++ client/src/redux/features/products/productApi.js
@@ -9,3 +9,14 @@ export function fetchAllProducts (){
         console.log(data)
     })
 }
+
+
+
+export function fetchProductById(id) {
+    return new Promise(async (resolve) => {
+      const response = await fetch(`http://localhost:5000/api/products/${id}`);
+      const data = await response.json();
+      console.log("id data", data)
+      resolve({ data });
+    });
+  }
--- client/src/redux/features/products/productsSlice.js
+++ client/src/redux/features/products/productsSlice.js
@@ -1,37 +1,49 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import axios from "axios"
-import { fetchAllProducts } from './productApi'
-
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchAllProducts, fetchProductById } from "./productApi";
 
 const initialState = {
-    products : [],
-    status : "idle"
-}
+  products: null,
+  status: "idle",
+  selectedProduct: null,
+};
 
-
+export const fetchProductByIdAsync = createAsyncThunk(
+  "product/fetchProductById",
+  async (id) => {
+    const response = await fetchProductById(id);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
 export const fetchAllProductAsync = createAsyncThunk(
-    'product/fetchAllProduct',
-  async() => {
-   const res =  await fetchAllProducts("http://localhost:5000/api/products")
-   return res.data
- })
+  "product/fetchAllProduct",
+  async () => {
+    const res = await fetchAllProducts("http://localhost:5000/api/products");
+    return res.data;
+  }
+);
 const productsSlice = createSlice({
-    name : 'product',
-    initialState,
-    reducers:{},
-    extraReducers: (builder) => {
-        builder
-        .addCase(fetchAllProductAsync.pending , (state) => {
-            state.status = 'loading'
-        })
-        .addCase(fetchAllProductAsync.fulfilled , (state, action) => {
-            state.status = 'success';
-            state.products = action.payload;    
-        })
-        
-    }
-})
-
-export const selectAllProducts = (state) => state.product.products
-export default productsSlice.reducer
+  name: "product",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProductAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAllProductAsync.fulfilled, (state, action) => {
+        state.status = "success";
+        state.products = action.payload;
+      })
+      .addCase(fetchProductByIdAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProductByIdAsync.fulfilled, (state, action) => {
+        state.status = "success";
+        state.selectedProduct = action.payload;
+      });
+  },
+});
+export const selectProductById = (state) => state.product.selectedProduct;
+export const selectAllProducts = (state) => state.product.products;
+export default productsSlice.reducer;
