e69be736e9e9a26e72af8010d58ccbc9f3872ef4
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-04 16:41:46 +0300

    ProductComponent

--- client/src/components/product/ProductComponent.js
+++ client/src/components/product/ProductComponent.js
@@ -1,28 +1,25 @@
-
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchAllProductAsync,
+  selectAllProducts,
+} from "../../redux/features/products/productsSlice";
 
-const ProductComponent = () => {
-    return (
-      <div className="w-36 border-2 m-4 h-36" >
-        <Link to={`/product/id`}>
-          <div className="ui link cards">
-            <div className="card">
-              <div className="image">
-                <img />
-              </div>
-              <div className="content">
-                <div className="header">title</div>
-                <div className="meta price">$ price</div>
-                <div className="meta">category</div>
-              </div>
-            </div>
-          </div>
+const ProductComponent = ({ product }) => {
+  return (
+    <div className="w-48  text-white border-2 m-4 h-68">
+        <Link to={`/product-details/${product._id}`}>
+          <div className="w-full h-[70%]">
+          <img className="w-full h-full object-cover " 
+          src={product.images} />
+                    </div>
+          <p className="justify-center flex">{product.name} </p>
+          <p className="flex justify-center mt-1"> ${product.price} </p>
+           <button className="hover:bg-red-500 flex justify-center mt-2 w-full ">Add to cart</button>
         </Link>
       </div>
-    );
+  );
 };
 
 export default ProductComponent;
