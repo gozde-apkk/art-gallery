dbc1a94174ecf0083e966baf167f4140e2d960e5
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-01-04 16:41:23 +0300

    ProductPage

---
+++ client/src/components/product/ProductPage.js
@@ -0,0 +1,30 @@
+
+
+
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
+import { fetchAllProductAsync, selectAllProducts } from "../../redux/features/products/productsSlice";
+
+const ProductPage = () => {
+
+  const products = useSelector(selectAllProducts)
+  const dispatch = useDispatch();
+   useEffect(() => {
+    dispatch(fetchAllProductAsync())
+    console.log("PRODUCT-PAGEEE",products);
+   },[dispatch])
+
+
+  return (
+    <div style={{paddingLeft:"72px" , flex:"wrap"}} className=" flex flex-wrap w-[68rem]">
+       {products.products && products.products.map((product) =>{
+         return <div className=" flex mr-2">
+          <ProductComponent key={product.id} product={product} />
+         </div>
+       })}
+    </div>
+  );
+};
+
+export default ProductPage;
