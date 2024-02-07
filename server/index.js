c514b244cdbcebcf7b2428e04acc69f1103f75d0
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-02-07 21:52:40 +0300

    Add .vercel to .gitignore, update devDependencies in package.json, add jwt.js file, update .env, update vercel.json, update index.js, update package.json, delete artist.js

--- server/index.js
+++ server/index.js
@@ -63,25 +63,18 @@ app.post("/api/create-checkout-session",async(req,res)=>{
   res.json({id:session.id})
 
 })
-
+const __dirname = path.resolve();   
 app.use("/api/products", require("./routes/productRouter.js"));
 app.use("/api/users" , require("./routes/userRoutes.js"));
 app.use('/categories', require("./routes/categoryRoute.js"));
 app.use('/api/stripe', stripe );
 
+app.use(express.static(path.join(__dirname, '/client/build')));
 
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-  
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    );
-  } else {
-    app.get('/', (req, res) => {
-      res.send('API is running....');
-    });
-  }
+app.get('*' , (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'))
+    
+})
 
   app.use(notFound);
   app.use(errorHandler);
